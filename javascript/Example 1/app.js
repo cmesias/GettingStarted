e177c1987aa3ee370fa40153b5638af10202afd2
Carl <carlmesias20@gmail.com>
2024-06-23 19:53:17 -0700

    add javascript example

---
+++ javascript/Example 1/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+let status_me = 'offline';
+
+let color = status_me === 'offline' ? 'red' : 'green';
+
+console.log('Color: ' + color);
+
+let dirtyEmail = '   jOnDoughnUT@gMAIL.cOM';
+
+let cleanedEmail = dirtyEmail.trim().toLowerCase();
+
+console.log('Dirty email: ' + dirtyEmail);
+console.log('Cleaned email: ' + cleanedEmail);
